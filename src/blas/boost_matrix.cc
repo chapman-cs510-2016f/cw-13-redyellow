6cd0e9bccd52b766fe6e736e05e52e50fe7e63e0
helen77 <zny_ok1989@hotmail.com>
2016-12-09 22:19:46 +0000

    changed boost_matrix.cc

--- src/blas/boost_matrix.cc
+++ src/blas/boost_matrix.cc
@@ -1,12 +1,26 @@
 #include <boost/numeric/ublas/matrix.hpp>
 #include <boost/numeric/ublas/io.hpp>
 
+// exploit namespaces to shorten code
+using namespace boost::numeric::ublas;
+using std::cout; 
+using std::endl;
+
+void print_plane(matrix<std::complex<long double> > p, int rows, int cols)
+{
+	int r,c;
+
+	for (r=0;r<rows;r++){
+		for (c=0;c<cols;c++){
+			cout<<p(r,c) << ' ';	
+		}
+		cout << endl;
+	}
+
+}
+
 int main(int argc, char **argv) 
 {
-  // exploit namespaces to shorten code
-  using namespace boost::numeric::ublas;
-  using std::cout; 
-  using std::endl;
 
   // declare three 3x3 matrices of complex<long double> elements
   matrix<std::complex<long double> > m(3, 3), n(3, 3), o(3, 3);
@@ -33,11 +47,14 @@ int main(int argc, char **argv)
   cout << endl << "n:" << endl;
   cout << n << endl;
   cout << endl << "o:" << endl;
-  cout << n << endl;
+  cout << o << endl;
   cout << endl << "m + n:" << endl;
   cout << m + n << endl;
   cout << endl << "m * n:" << endl;
   cout << prod(m, n) << endl;
   cout << endl << "n * n - o:" << endl;
   cout << prod(n, n) - o << endl;
+  cout << endl;
+  cout << "Print matrix m as a plane:" << endl;
+  print_plane(m,3,3);
 }
